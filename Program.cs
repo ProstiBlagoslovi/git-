19674013f4307a78f9c86a3dcc2863d100093f72
ParaDIEse <verashchelchkova@gmail.com>
2024-10-21 11:31:48 +0300

    Длина окружности

--- Program.cs
+++ Program.cs
@@ -22,6 +22,8 @@ namespace gitкр
         double distance = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
         Console.WriteLine("Расстояние между двумя точками: {0}", distance);
 
+        //длина окружности с радиусом равным расстоянию точек
+
             Console.ReadLine();
         }
 }
